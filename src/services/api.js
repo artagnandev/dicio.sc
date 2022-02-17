0a66974506d84d090ea9c52eb81d4c1931d971de
artagnandev <macmini@artagnandev.local>
2022-02-16 22:45:33 -0300

    api linked

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://discio-api.herokuapp.com/',
   headers: {
     'content-type': 'application/json',
     'Access-Control-Allow-Origin': '*'
